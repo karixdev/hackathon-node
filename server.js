26ba64e7156f4dd32bcbcb9dcdb392f716e98089
karixdev <kari.klucz@gmail.com>
2023-12-02 20:48:22 +0100

    feat: add factors endpoints

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import {} from 'dotenv/config';
+import { parameterFactorRouter } from './controller/parameterFactor.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use('/factors', parameterFactorRouter);
 
 app.listen(port, () => {
     console.log(`app working on: http://localhost:${port}`);
